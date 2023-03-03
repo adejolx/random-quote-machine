18936fb09acbfeaab3ffd12ce3be225ef8a18bea
adejolx <recursr.dev@gmail.com>
2023-03-04 00:24:35 +0100

    Add default quote and author text with styles

--- src/components/QuoteDisplay.tsx
+++ src/components/QuoteDisplay.tsx
@@ -7,21 +7,22 @@ type QuoteDisplayProps = {
 };
 
 export default function QuoteDisplay({ quote, color }: QuoteDisplayProps) {
-  const isTrue = true;
+  const defaultQuote = "You're the writer of your own destiny";
+  const defaultAuthor = "Anonymous";
   return (
     <>
       <motion.div
         className="stack-s"
         key={color}
-        initial={{ opacity: 0 }}
+        initial={{ opacity: 0.5 }}
         animate={{ color: `${color}`, opacity: 1 }}
         transition={{ duration: 0.5, ease: "easeOut" }}
       >
         <p id="text" className="font-large bold quote center-text">
-          {quote.quote}
+          {quote.quote ? quote.quote : defaultQuote}
         </p>
         <span id="author" className="block right-text italicize">
-          - {quote.author}
+          - {quote.author ? quote.author : defaultAuthor}
         </span>
       </motion.div>
     </>
